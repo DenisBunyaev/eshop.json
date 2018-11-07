3cc7465ff6df96b8c1406e8fc9f2106ac66565d8
Denis Olegovich <deniska@example.ua>
2018-11-07 17:41:11 +0200

    start making goods cart

--- js/eshop.js
+++ js/eshop.js
@@ -1,3 +1,5 @@
+var cart={}; //моя корзина (массив)
+
 $( 'document' ).ready(function() 
 {
  loadgoods();   
@@ -16,10 +18,30 @@ function loadgoods()
             out+='<h3>'+data[key]['name']+'</h3>';
             out+='<p>Цена: '+data[key]['cost']+" грн"+'</p>';
             out+='<img src="'+data[key].image+'">';
-            out+='<button>Купить</button>';
+            out+='<button class="add-to-cart" data-art="'+key+'">Купить</button>';
             out+='</div>';
             
         }
         $('#goods').html(out);
-    })
+        $('button.add-to-cart').on('click', addToCart);
+    });
 }
+
+function addToCart()
+{
+    //добавление товара в корзину через lockal storage
+    var articul = $(this).attr('data-art');
+    if(cart[articul]!=undefined)
+   {
+       cart[articul]++;
+   } else {
+       cart [articul] = 1;
+   }
+    console.log(cart);
+}
+
+
+
+
+
+
