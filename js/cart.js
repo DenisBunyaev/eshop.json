8f6a2eee9b9e67e1bb88af947d1cd467f53d29fb
Denis Olegovich <deniska@example.ua>
2018-11-08 16:55:26 +0200

    adding functional for some buttoms

--- js/cart.js
+++ js/cart.js
@@ -15,17 +15,50 @@ $.getJSON('goods.json', function(data){
             // out+= key + ' - кол-во - '+cart[key]+'<br>';
             // простой вывод цифр
             // далее графическое представление
-            out += '<button class="delete">x</button>'+" ";
+            out += '<button class="delete" data-art="'+key+'">x</button>'+" ";
             out += '<img src="'+goods[key].image+'" width="65">'+" ";
             out += goods[key].name+" ";
-            out += '<button class="minus">-</button>'+" ";
+            out += '<button class="minus" data-art="'+key+'">-</button>'+" ";
             out += cart[key]+" ";
-            out += '<button class="plus">+</button>'+" ";
+            out += '<button class="plus" data-art="'+key+'">+</button>'+" ";
             out += " Итого: "+cart[key]*goods[key].cost;
             out += '<br />';
         }
         $('#my-cart').html(out);
+        $('.plus').on('click', plusGoods);
+        $('.minus').on('click', minusGoods);
+        $('.delete').on('click', deleteGoods);
     }
+    
+    function plusGoods()
+    {
+        var articul = $(this).attr('data-art');
+        cart[articul]++;
+        saveCartToLS(); //Сохранение корзины в local storage
+        showCart();
+    }
+    
+    function minusGoods()
+    {
+        var articul = $(this).attr('data-art');
+        if (cart[articul]>1) 
+        {
+             cart[articul]--;
+        } else {
+            delete cart[articul];
+        }   
+        saveCartToLS(); //Сохранение корзины в local storage
+        showCart();
+    }
+    
+    function deleteGoods()
+    {
+        var articul = $(this).attr('data-art');
+        delete cart[articul];
+        saveCartToLS(); //Сохранение корзины в local storage
+        showCart();
+    }
+
 });
 
 function checkCart()
@@ -36,4 +69,9 @@ function checkCart()
    {
        cart = JSON.parse ( localStorage.getItem('cart') );
    }
+}
+
+function saveCartToLS()
+{
+    localStorage.setItem( 'cart', JSON.stringify(cart) );
 }
